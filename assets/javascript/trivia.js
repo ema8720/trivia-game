6924e58f755892701af676d82a29ab1490bef36a
Juan Correa <ema8720@gmail.com>
2018-01-07 02:41:22 -0800

    revising next question function

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -92,7 +92,14 @@ var game = {
         game.loadQuestion();
     },
     timeUp: function() {
-
+        clearInterval(timer);
+        $('#subwrapper').html('<h2>OUT OF TIME!</h2>');
+        $('#subwrapper').append('<h3>The correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     results: function() {
 
@@ -109,10 +116,24 @@ var game = {
         console.log("You Got it!");
         clearInterval(timer);
         game.correct++;
-        $("#subwrapper").html('<h2>YOU GOT IT RIGHT</h2>')
+        $("#subwrapper").html('<h2>YOU GOT IT RIGHT</h2>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     answeredIncorrectly: function() {
         console.log("Wrong!");
+        clearInterval(timer);
+        game.incorrect++;
+        $("#subwrapper").html('<h2>YOU GOT IT WRONG!</h2>');
+        $('#subwrapper').append('<h3>The correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     reset: function() {
 
